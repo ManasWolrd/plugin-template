f3861593066e7891dc3a5039eb1451af4328800e
a5632645 <3022602213@qq.com>
2026-02-26 10:17:52 +0800

    update

--- src/global.hpp
+++ src/global.hpp
@@ -2,7 +2,7 @@
 
 namespace global {
 
-static constexpr auto kPluginRepoOwnerName = "ManasWorld";
+static constexpr auto kPluginRepoOwnerName = "ManasWolrd";
 static constexpr auto kPluginRepoName = "plugin-template";
 
 }
